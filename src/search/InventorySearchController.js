f96a0dc2b89458237582da4745a9710a17621ee0
srldl <siri.uldal@gmail.com>
2016-01-13 16:44:57 +0100

    formula.json update

--- src/search/InventorySearchController.js
+++ src/search/InventorySearchController.js
@@ -7,23 +7,21 @@ var InventorySearchController = function ($scope, $location, $controller, Invent
   $controller('NpolarBaseController', { $scope: $scope });
   $scope.resource = Inventory;
 
-  npdcAppConfig.search.local.results.detail = (e) => {
-    return "Activity start: " + e.activity[0].departed.split('T')[0];
-   };
+ /* npdcAppConfig.search.local.results.detail = function (entry) {
+    return "Released: " + (entry.released ? entry.released.split('T')[0] : '-');
+  }; */
+
+  npdcAppConfig.search.local.results.detail = function (entry) {
+    return entry.category + ' - ' + entry.description;
+  };
+
 
   npdcAppConfig.cardTitle = "Inventory Archive";
   npdcAppConfig.search.local.results.subtitle = "type";
-  npdcAppConfig.search.local.filterUi = {
-    'year-activity.departed': {
-      type: 'range'
-    },
-    'updated': {
-      type: 'hidden'
-    }
-  };
+
 
   let query = function() {
-    let defaults = { limit: "all", sort: "-updated", fields: 'category,instrument,description',
+    let defaults = { limit: "all", sort: "-updated", fields: 'id,category,instrument,description',
       'date-year': 'release_date', facets: 'category,instrument' };
     let invariants = $scope.security.isAuthenticated() ? {} : {} ;
     return Object.assign({}, defaults, invariants);
